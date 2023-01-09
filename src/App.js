9431a19c7835cf0ccd5044f08309e3d541b66ef2
reski-id <pesan.reski@gmail.com>
2023-01-09 22:57:19 +0700

    tugas W2D1 : Redux

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+import "./App.css";
+
+import TodoComponent from "./components/TodoComponent";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, removeTodo } from "./features/todoSlice";
+
+function App() {
+  const [input, setInput] = useState("");
+
+  const count = useSelector((state) => state.todo.count);
+  const todos = useSelector((state) => state.todo.todos);
+  const dispatch = useDispatch();
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+  };
+
+  const handleTodoDone = (id) => {
+    dispatch(removeTodo(id));
+  };
+  return (
+    <div className="App">
+      <h1>think Todo</h1>
+      <form className="App-form" onSubmit={handleAddTodo}>
+        <input type="text" onInput={(e) => setInput(e.target.value)} />
+        <button type="submit">+</button>
+      </form>
+      <div className="Todos">
+        {count > 0 &&
+          todos.map((todo) => (
+            <TodoComponent
+              key={todo.id}
+              text={todo.text}
+              id={todo.id}
+              onCheck={handleTodoDone}
+            />
+          ))}
+        {count === 0 && <p>No ongoing Task</p>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
